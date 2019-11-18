dfed43dc028e4a811ac9756e68fba406954c2c88
RalucaCovrig <46185194+RalucaCovrig@users.noreply.github.com>
2019-11-18 09:40:08 +0200

    test

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -16,7 +16,7 @@ namespace StarChart.Controllers
 
         public CelestialObjectController(ApplicationDbContext context)
         {
-            this._context = context;
+            this._context = context; 
         }
     }
 }
